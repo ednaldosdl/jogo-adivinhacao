4b43dba98d55cadb59b2ac6a12d3feacc3efaeb4
Edinaldo Soares <ednaldosdl@gmail.com>
2023-08-21 01:38:58 -0300

    Finalização do jogo.

--- src/bin/capitulo_02/main.rs
+++ src/bin/capitulo_02/main.rs
@@ -1,3 +1,33 @@
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Olá, esse é o main do capitulo 02.");
+    println!("Adivinhe o núemro!");
+
+    let valor_secreto: u32 = rand::thread_rng().gen_range(1..101);
+    println!("O número secreto é: {}", valor_secreto);
+
+    loop {    
+        println!("Por favor informe um número.");
+        let mut adivinhar: String = String::new();
+        io::stdin().read_line(&mut adivinhar).expect("Falha ao ler a entrada");
+
+        println!("você adivinhou: {adivinhar}");
+
+        let adivinhar:u32 = match adivinhar.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Seu palpite: {}", adivinhar);    
+        match adivinhar.cmp(&valor_secreto) {
+            Ordering::Less => println!("Muito pequeno!"),
+            Ordering::Greater => println!("Muito grande!"),
+            Ordering::Equal => {
+                println!("Você ganhou!");
+                break;
+            }
+        }
+    }
 }
