dac421be26d0bb8449cfa144c0e514881daafd0e
Edinaldo Soares <ednaldosdl@gmail.com>
2023-08-24 13:44:16 -0300

    Trabalhando com as variáveis no Rust.

---
+++ src/bin/capitulo_03/main.rs
@@ -0,0 +1,21 @@
+//extern crate rand;
+
+//use std::io;
+//use std::cmp::Ordering;
+//use rand::Rng;
+
+fn main() {
+
+  let x: i32 = 50;
+  println!("O valor de x é: {}", x);
+  
+  let x = 60;
+  println!("O valor de x é: {}", x);
+
+  let y = x;
+  println!("O valor de y é: {}", y);
+  
+  const PI: f32 = 3.14;
+  println!("O valor de PI é: {}", PI);
+
+}
